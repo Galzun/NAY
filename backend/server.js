ca4a353510a34d322288723bab22bf0639eda8a3
Galzun <golzen_lol@mail.ru>
2025-12-02 00:52:39 +0300

    Проверка 4

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,13 @@ import authRoutes from "./routes/auth.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const allowedOrigins = [
-    "http://localhost:3000",
-    "https://galzun-nay-9d8c.twc1.net" // домен фронта из Apps https://galzun-nay-c390.twc1.net
-];
+// const allowedOrigins = [
+//     "http://localhost:3000",
+//     "https://galzun-nay-9d8c.twc1.net" // домен фронта из Apps
+// ];
 
-app.use(cors({origin: allowedOrigins,}));
+app.use(cors());
+// app.use(cors({origin: allowedOrigins,}));
 app.use(express.json());
 
 await connectDB(); // Node 24 supports top-level await in ESM
