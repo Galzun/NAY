069078f2ebf2d1ce8c39854b9bc7974d6af3d685
Galzun <golzen_lol@mail.ru>
2025-12-05 01:31:06 +0300

    Аватар 8

--- src/Components/Static/streamerImages.js
+++ src/Components/Static/streamerImages.js
@@ -1,7 +1,7 @@
 // All.js
 const streamerImages = {
-    "5opka": "public/avatar/50pka.webp",
-    "Cs": "public/avatar/counter-strike.webp",
+    "5opka": "./public/avatar/50pka.webp",
+    "Cs": "./public/avatar/counter-strike.webp",
 };
 
 export default streamerImages;
