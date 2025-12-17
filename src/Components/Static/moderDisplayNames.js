bfacc77cfd2dba7f7e55a68a17348a04c720a096
Galzun <golzen_lol@mail.ru>
2025-12-17 18:35:13 +0300

    VaOne

--- src/Components/Static/moderDisplayNames.js
+++ src/Components/Static/moderDisplayNames.js
@@ -4,7 +4,7 @@ const moderDisplayNames = {
     "Gargamel_mod": "Gargamel",
     "VaOne_mod": "VaOne",
     "Lx_mod": "lxtdxwn",
-    "Тревор_mod": "Тревор Шарп",
+    "Trevor_mod": "Тревор Шарп",
 };
 
 export default moderDisplayNames;
